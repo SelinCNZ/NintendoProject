4d62f9eba137c3a2b2b89e5b6469147d2c445aea
SelinCNZ <selin.cengiz.tr@gmail.com>
2021-10-11 17:11:07 +0200

    Creation main

---
+++ src/main/java/metier/TestMain.java
@@ -0,0 +1,16 @@
+package metier;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		Jeu j1 = new Jeu ("Call Of Duty",);
+		Jeu j2 = new Jeu ("Mario Kart",);
+		Jeu j3 = new Jeu ("Pokemon",);
+		Jeu j4 = new Jeu ("Naruto",);
+		Jeu j5 = new Jeu ("Bataille Navale",);
+		
+
+	}
+
+}

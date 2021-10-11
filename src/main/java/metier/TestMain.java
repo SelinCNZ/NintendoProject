6c3512e4b5ada7675f5b6b4f675f8aefaced965d
Biscay <hugo.bcpst@gmail.com>
2021-10-11 17:53:25 +0200

    addBouMain

--- src/main/java/metier/TestMain.java
+++ src/main/java/metier/TestMain.java
@@ -1,5 +1,8 @@
 package metier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -9,13 +12,23 @@ public class TestMain {
 		Console c2 = new Console ("Playstation");
 		Console c3 = new Console ("XBox");
 		
-		Jeu j1 = new Jeu ("Call Of Duty",c1);
-		Jeu j2 = new Jeu ("Mario Kart",c1);
-		Jeu j3 = new Jeu ("Pokemon",c3);
-		Jeu j4 = new Jeu ("Naruto",c2);
-		Jeu j5 = new Jeu ("Bataille Navale",c2);
+		List <Console> consoles = new ArrayList();
 		
-
+		consoles.add(c3);
+		consoles.add(c2);
+		consoles.add(c1);
+		
+		Jeu j1 = new Jeu ("Call Of Duty",consoles);
+		Jeu j2 = new Jeu ("Mario Kart",consoles);
+		Jeu j3 = new Jeu ("Pokemon",consoles);
+		Jeu j4 = new Jeu ("Naruto",consoles);
+		Jeu j5 = new Jeu ("Bataille Navale",consoles);
+		
+		
+		List<Jeu> jeux= new ArrayList<Jeu>();
+		
+		
+		//Boutique b= new Boutique ()
 	}
 
 }

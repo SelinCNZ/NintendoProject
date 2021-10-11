81613589ee2b21b3ea02ae98bc94d620238fb41e
SelinCNZ <selin.cengiz.tr@gmail.com>
2021-10-11 17:47:34 +0200

    Addjeumodif

--- src/main/java/metier/TestMain.java
+++ src/main/java/metier/TestMain.java
@@ -1,19 +1,30 @@
 package metier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		
 		
 		Console c1 = new Console ("Nintendo");
 		Console c2 = new Console ("Playstation");
 		Console c3 = new Console ("XBox");
 		
-		Jeu j1 = new Jeu ("Call Of Duty",c1);
-		Jeu j2 = new Jeu ("Mario Kart",c1);
-		Jeu j3 = new Jeu ("Pokemon",c3);
-		Jeu j4 = new Jeu ("Naruto",c2);
-		Jeu j5 = new Jeu ("Bataille Navale",c2);
+		
+		
+		List <Console> consoles = new ArrayList();
+		consoles.add(c1);
+		consoles.add(c2);
+		consoles.add(c3);
+		
+		
+		
+		//Jeu j1 = new Jeu ("Call Of Duty",c1);
+		//Jeu j2 = new Jeu ("Mario Kart",c1);
+		//Jeu j3 = new Jeu ("Pokemon",c3);
+		//Jeu j4 = new Jeu ("Naruto",c2);
+		//Jeu j5 = new Jeu ("Bataille Navale",c2);
 		
 
 	}

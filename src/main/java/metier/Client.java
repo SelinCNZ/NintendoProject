3731a0a7685456735b172ec8f03fc8cb7301b61e
Biscay <hugo.bcpst@gmail.com>
2021-10-11 17:31:22 +0200

    Add client

--- src/main/java/metier/Client.java
+++ src/main/java/metier/Client.java
@@ -1,5 +1,42 @@
 package metier;
 
+import java.util.List;
+
 public class Client {
 
+	protected String nom;
+	protected String prenom;
+	protected List <Jeu> jeu;
+	
+	public Client(String nom, String prenom, List<Jeu> jeu) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.jeu = jeu;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [nom=" + nom + ", prenom=" + prenom + ", jeu=" + jeu + "]";
+	}
+
+	
+	
+	
 }
